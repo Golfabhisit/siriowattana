1a1eeb994e0de3bc178492d6daad667705dfad89
Golfabhisit <51833579+Golfabhisit@users.noreply.github.com>
2019-06-15 14:53:07 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'zBuEK1i5oQygQiIIFcEop3wypmfXkwR06lTQZE+jNHBeqBQpwgue8py0xNnrbyE7oBGor2sBcmR8ddT4HYtcjoWZ6ccUxf+tRLJjW1dTvN4UsAoP1F6nWacB+HNt3uhBw4aeNuH4ePkiYU8l2QDziAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
